62732332bc5d169df0be475a3840847e3ff670c8
Ram <ramanjaneyulu901@gmail.com>
2019-11-29 02:25:24 +0530

    Landing Page UI

---
+++ assets/js/commonscripts.js
@@ -0,0 +1,20 @@
+
+$('#testimonials').owlCarousel({
+    center: true,
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      600: {
+        items: 3
+      }
+    }
+  });
+
+  $('#paymentSolutions').owlCarousel({
+    margin: 10,
+    nav: false,
+    dots: true,
+    items: 1
+  });
