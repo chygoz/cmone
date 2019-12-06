7fba15ddf35d6c31014ac08ae19012d2e2169419
Ram <ramanjaneyulu901@gmail.com>
2019-12-06 23:11:39 +0530

    Ui Fixes and alignments

--- assets/js/commonscripts.js
+++ assets/js/commonscripts.js
@@ -5,6 +5,10 @@ $('#testimonials').owlCarousel({
     margin: 10,
     nav: true,
     dots: false,
+    navText: [
+			'<span aria-label="' + 'Previous' + '"><i class="fa fa-angle-left" aria-hidden="true"></i></span>',
+			'<span aria-label="' + 'Next' + '"><i class="fa fa-angle-right" aria-hidden="true"></i></span>'
+		],
     responsive: {
       600: {
         items: 3
